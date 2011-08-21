b05112e5360f88d963418550afa1ac3e969e816b
Benson Peng <penglingzhe@gmail.com>
2011-08-21 09:14:33 +0800

    add addShoppingItemService

--- src/com/orange/groupbuy/dao/User.java
+++ src/com/orange/groupbuy/dao/User.java
@@ -1,6 +1,46 @@
 package com.orange.groupbuy.dao;
 
+import java.util.Date;
+
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.orange.common.utils.StringUtil;
+import com.orange.groupbuy.constant.DBConstants;
+
 public class User extends CommonData {
 
+	public User(DBObject dbObject){
+		super(dbObject);
+	}
+
+	public boolean addShoppingItem(String itemId, String categoryName, String subCategoryName,
+			String keywords, double maxPrice, double minRebate) {
+		
+		BasicDBObject item = new BasicDBObject();		
+		
+		if (!StringUtil.isEmpty(categoryName))
+			item.put(DBConstants.F_CATEGORY_NAME, categoryName);
+		if (!StringUtil.isEmpty(subCategoryName))
+			item.put(DBConstants.F_SUB_CATEGORY_NAME, subCategoryName);
+		if (!StringUtil.isEmpty(keywords))
+			item.put(DBConstants.F_KEYWORD, keywords);
+		if (maxPrice >= 0.0f)
+			item.put(DBConstants.F_MAX_PRICE, maxPrice);
+		if (minRebate >= 0.0f)
+			item.put(DBConstants.F_MIN_REBATE, minRebate);
+		
+		item.put(DBConstants.F_CREATE_DATE, new Date());
+		
+		BasicDBList shoppingList = (BasicDBList)dbObject.get(DBConstants.F_SHOPPING_LIST);
+		if (shoppingList == null){
+			shoppingList = new BasicDBList();
+			dbObject.put(DBConstants.F_SHOPPING_LIST, shoppingList);
+		}
+		
+		shoppingList.add(item);				
+		return true;
+	}
+	
 	
 }
--- src/com/orange/groupbuy/manager/UserManager.java
+++ src/com/orange/groupbuy/manager/UserManager.java
@@ -25,6 +25,7 @@ import com.orange.common.utils.DateUtil;
 import com.orange.common.utils.StringUtil;
 import com.orange.groupbuy.constant.DBConstants;
 import com.orange.groupbuy.dao.Gps;
+import com.orange.groupbuy.dao.User;
 
 public class UserManager extends CommonManager{
 	
@@ -32,14 +33,8 @@ public class UserManager extends CommonManager{
 		if (mongoClient == null || deviceId == null || deviceId.length() <= 0)
 			return null;
 		
-		BasicDBObject query = new BasicDBObject();
-		query.put(DBConstants.F_DEVICEID, deviceId);
-		DBCollection collection = mongoClient.getDb().getCollection(DBConstants.T_USER);
-		DBCursor cursor = collection.find(query);
-		if (cursor == null || cursor.hasNext() == false)
-			return null;
-		
-		return cursor.next();		
+		DBObject obj = mongoClient.findOne(DBConstants.T_USER, DBConstants.F_DEVICEID, deviceId);
+		return obj;
 	}
 	
 	public static DBObject findUserByEmail(MongoDBClient mongoClient, String email) {
@@ -65,14 +60,14 @@ public class UserManager extends CommonManager{
 		DBCollection collection = mongoClient.getDb().getCollection(DBConstants.T_USER);
 		DBCursor cursor = collection.find(query);
 		if (cursor == null || cursor.hasNext() == false)
-			return null;
+			return null;				
 		
 		return cursor.next();
 	}
 	
 	public static void updateStatusByVerifyCode(MongoDBClient mongoClient, String sta, String vcd){
 		if (mongoClient == null || sta == null || sta.length() <= 0 || vcd == null || vcd.length() <= 0)
-			return;
+			return;				
 		
 		BasicDBObject query = new BasicDBObject();
 		query.put(DBConstants.F_VERIFYCODE, vcd);
@@ -232,5 +227,68 @@ public class UserManager extends CommonManager{
 		
 		System.out.println("<addSearchKeyword> query="+query.toString()+",value="+pushValue);
 		mongoClient.updateAll(DBConstants.T_USER, query, pushValue);
+	}
+
+	public static User findUserByUserId(MongoDBClient mongoClient, String userId) {
+		if (mongoClient == null || userId == null || userId.length() <= 0)
+			return null;
+		
+		DBObject obj = mongoClient.findOne(DBConstants.T_USER, DBConstants.F_USERID, userId);
+		if (obj == null)
+			return null;
+		
+		return new User(obj);
+	}
+
+	public static boolean save(MongoDBClient mongoClient, User user) {
+		mongoClient.save(DBConstants.T_USER, user.getDbObject());
+		return true;
+	}
+	
+	private static BasicDBObject createItem(String itemId,
+			String categoryName, String subCategoryName, String keywords,
+			double maxPrice, double minRebate){
+		
+		if (StringUtil.isEmpty(itemId))
+			return null;
+
+		BasicDBObject item = new BasicDBObject();		
+		item.put(DBConstants.F_ITEM_ID, itemId);
+		item.put(DBConstants.F_CREATE_DATE, new Date());	
+		
+		if (!StringUtil.isEmpty(categoryName))
+			item.put(DBConstants.F_CATEGORY_NAME, categoryName);
+		if (!StringUtil.isEmpty(subCategoryName))
+			item.put(DBConstants.F_SUB_CATEGORY_NAME, subCategoryName);
+		if (!StringUtil.isEmpty(keywords))
+			item.put(DBConstants.F_KEYWORD, keywords);
+		if (maxPrice >= 0.0f)
+			item.put(DBConstants.F_MAX_PRICE, maxPrice);
+		if (minRebate >= 0.0f)
+			item.put(DBConstants.F_MIN_REBATE, minRebate);
+		
+		return item;
+	}
+
+	public static boolean addUserShoppingItem(MongoDBClient mongoClient, String userId, String itemId,
+			String categoryName, String subCategoryName, String keywords,
+			double maxPrice, double minRebate) {
+		
+		BasicDBObject item = createItem(itemId, categoryName, 
+				subCategoryName, keywords, maxPrice, minRebate);
+		if (item == null)
+			return false;
+
+		BasicDBObject query = new BasicDBObject();
+		query.put(DBConstants.F_USERID, userId);
+
+		BasicDBObject pushValue = new BasicDBObject();		
+		pushValue.put(DBConstants.F_SHOPPING_LIST, item);
+
+		BasicDBObject update = new BasicDBObject();				
+		update.put("$push", pushValue);
+		
+		mongoClient.updateAll(DBConstants.T_USER, query, update);		
+		return true;
 	}
 }
