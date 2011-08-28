4a43724f86a2c49a05304506126bd140a55ffdd5
Lizhang Peng <penglzh2010@qq.com>
2011-08-28 13:14:47 +0800

    add up/down

--- src/com/orange/groupbuy/constant/DBConstants.java
+++ src/com/orange/groupbuy/constant/DBConstants.java
@@ -331,6 +331,9 @@ public class DBConstants {
 	public static final String F_ITEM_ID = "item_id";
 
     public static final float MIN_SCORE_TO_RECOMMEND = 0.2f;
+
+    public static final String F_UP = "up";
+    public static final String F_DOWN = "down";
     
     // test
 }
